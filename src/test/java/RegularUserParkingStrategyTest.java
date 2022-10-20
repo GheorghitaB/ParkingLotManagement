cdc9d19c67ae4b61d31a1faac8fc39e32e2d101c
Gheorghita Butnaru <gheorghita.butnaru@basware.com>
2022-10-20 14:53:35 +0300

    Added unit tests

--- src/test/java/RegularUserParkingStrategyTest.java
+++ src/test/java/RegularUserParkingStrategyTest.java
@@ -1,145 +1,105 @@
 import exceptions.ParkingSpotNotFound;
-import exceptions.UnknownVehicleType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import parkinglots.ParkingLotInMemoryRepository;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import parkinglots.ParkingLotRepository;
-import parkingspots.LargeParkingSpot;
-import parkingspots.MediumParkingSpot;
-import parkingspots.ParkingSpot;
-import parkingspots.SmallParkingSpot;
+import parkingspots.*;
 import parkingstrategies.RegularUserParkingStrategy;
 import vehicles.Car;
 import vehicles.Motorcycle;
 import vehicles.Truck;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
-
-public class RegularUserParkingStrategyTest {
-    private RegularUserParkingStrategy regularUserParkingStrategy;
+public class RegularUserParkingStrategyTest{
+    @Mock
     private ParkingLotRepository parkingLotRepository;
-    List<ParkingSpot> parkingSpots;
-    final String PLATE_NUMBER = "FAKE";
+    @InjectMocks
+    private RegularUserParkingStrategy regularUserParkingStrategy;
 
     @BeforeEach
-    void initEach(){
+    void init(){
+        MockitoAnnotations.initMocks(this);
         regularUserParkingStrategy = new RegularUserParkingStrategy();
-        parkingSpots = new ArrayList<>();
-        parkingLotRepository = new ParkingLotInMemoryRepository(parkingSpots);
     }
 
     @Test
-    void getParkingSpotShouldThrowExceptionWhenNoParkingSpots(){
-        assertThrows(ParkingSpotNotFound.class, ()-> regularUserParkingStrategy.getParkingSpot(new Car(PLATE_NUMBER, false),
-                parkingLotRepository));
+    void shouldThrowParkingSpotNotFoundWhenThereIsNoEmptyParkingSpot() throws ParkingSpotNotFound {
+        when(parkingLotRepository.getEmptyParkingSpotOfType(ParkingSpotType.SMALL)).thenThrow(ParkingSpotNotFound.class);
+        assertThrows(ParkingSpotNotFound.class,
+                ()-> regularUserParkingStrategy.getParkingSpot(new Motorcycle("", false), parkingLotRepository));
+        verify(parkingLotRepository, times(1)).getEmptyParkingSpotOfType(ParkingSpotType.SMALL);
     }
 
     @Test
-    void getParkingSpotShouldReturnSmallParkingSpot() throws ParkingSpotNotFound {
-        parkingSpots.add(new SmallParkingSpot(false));
+    void shouldReturnSmallParkingSpotForMotorcycleWhenThereIsAnEmptySmallParkingSpot() throws ParkingSpotNotFound {
+        when(parkingLotRepository.getEmptyParkingSpotOfType(ParkingSpotType.SMALL)).thenReturn(new SmallParkingSpot(false));
 
-        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Motorcycle(PLATE_NUMBER, false),
-                parkingLotRepository);
+        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Motorcycle("", false), parkingLotRepository);
 
-        assertEquals(SmallParkingSpot.class, parkingSpot.getClass());
+        verify(parkingLotRepository, times(1)).getEmptyParkingSpotOfType(ParkingSpotType.SMALL);
+        assertEquals(ParkingSpotType.SMALL, parkingSpot.getParkingSpotType());
     }
 
     @Test
-    void getParkingSpotShouldReturnMediumParkingSpot() throws ParkingSpotNotFound {
-        parkingSpots.add(new SmallParkingSpot(false));
-        parkingSpots.add(new MediumParkingSpot(false));
-        parkingSpots.add(new LargeParkingSpot(false));
+    void shouldReturnSmallParkingSpotWithElectricChargerForElectricMotorcycleWhenThereIsAnEmptySmallParkingSpotWithElectricCharger() throws ParkingSpotNotFound {
+        when(parkingLotRepository.getEmptyParkingSpotWithElectricChargerOfType(ParkingSpotType.SMALL)).thenReturn(new SmallParkingSpot(true));
 
-        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Car(PLATE_NUMBER, false),
-                parkingLotRepository);
+        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Motorcycle("", true), parkingLotRepository);
 
-        assertEquals(MediumParkingSpot.class, parkingSpot.getClass());
-    }
-
-    @Test
-    void getParkingSpotShouldReturnLargeParkingSpot() throws ParkingSpotNotFound {
-        parkingSpots.add(new SmallParkingSpot(false));
-        parkingSpots.add(new MediumParkingSpot(false));
-        parkingSpots.add(new LargeParkingSpot(false));
-
-        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Truck(PLATE_NUMBER, false),
-                parkingLotRepository);
-
-        assertEquals(LargeParkingSpot.class, parkingSpot.getClass());
-    }
-
-    @Test
-    void getParkingSpotShouldReturnSmallParkingSpotWithElectricCharger() throws ParkingSpotNotFound {
-        parkingSpots.add(new SmallParkingSpot(false));
-        parkingSpots.add(new SmallParkingSpot(true));
-
-        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Motorcycle(PLATE_NUMBER, true),
-                parkingLotRepository);
-
-        assertEquals(SmallParkingSpot.class, parkingSpot.getClass());
         assertEquals(true, parkingSpot.hasElectricCharger());
+        assertEquals(ParkingSpotType.SMALL, parkingSpot.getParkingSpotType());
+        verify(parkingLotRepository, times(1)).getEmptyParkingSpotWithElectricChargerOfType(ParkingSpotType.SMALL);
     }
 
     @Test
-    void getParkingSpotShouldReturnMediumParkingSpotWithElectricCharger() throws ParkingSpotNotFound{
-        parkingSpots.add(new MediumParkingSpot(false));
-        parkingSpots.add(new MediumParkingSpot(true));
+    void shouldReturnMediumParkingSpotWhenThereIsAnEmptyMediumParkingSpot() throws ParkingSpotNotFound {
+        //given
+        when(parkingLotRepository.getEmptyParkingSpotOfType(ParkingSpotType.MEDIUM)).thenReturn(new MediumParkingSpot(false));
+        //when
+        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Car("", false), parkingLotRepository);
+        //then
+        assertEquals(ParkingSpotType.MEDIUM, parkingSpot.getParkingSpotType());
+        verify(parkingLotRepository, times(1)).getEmptyParkingSpotOfType(ParkingSpotType.MEDIUM);
+    }
 
-        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Car(PLATE_NUMBER, true),
-                parkingLotRepository);
-
-        assertEquals(MediumParkingSpot.class, parkingSpot.getClass());
+    @Test
+    void shouldReturnMediumParkingSpotWithElectricChargerWhenThereIsAnEmptyMediumParkingSpotWithElectricCharger() throws ParkingSpotNotFound {
+        //given
+        when(parkingLotRepository.getEmptyParkingSpotWithElectricChargerOfType(ParkingSpotType.MEDIUM)).thenReturn(new MediumParkingSpot(true));
+        //when
+        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Car("", true), parkingLotRepository);
+        //then
         assertEquals(true, parkingSpot.hasElectricCharger());
+        assertEquals(ParkingSpotType.MEDIUM, parkingSpot.getParkingSpotType());
+        verify(parkingLotRepository, times(1)).getEmptyParkingSpotWithElectricChargerOfType(ParkingSpotType.MEDIUM);
     }
 
     @Test
-    void getParkingSpotShouldReturnLargeParkingSpotWithElectricCharger() throws ParkingSpotNotFound{
-        parkingSpots.add(new LargeParkingSpot(false));
-        parkingSpots.add(new LargeParkingSpot(true));
+    void shouldReturnLargeParkingSpotWhenThereIsAnEmptyLargeParkingSpot() throws ParkingSpotNotFound {
+        //given
+        when(parkingLotRepository.getEmptyParkingSpotOfType(ParkingSpotType.LARGE)).thenReturn(new LargeParkingSpot(false));
+        //when
+        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Truck("", false), parkingLotRepository);
+        //then
+        assertEquals(ParkingSpotType.LARGE, parkingSpot.getParkingSpotType());
+        verify(parkingLotRepository, times(1)).getEmptyParkingSpotOfType(ParkingSpotType.LARGE);
+    }
 
-        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Truck(PLATE_NUMBER, true),
-                parkingLotRepository);
-
-        assertEquals(LargeParkingSpot.class, parkingSpot.getClass());
+    @Test
+    void shouldReturnLargeParkingSpotWithElectricChargerWhenThereIsAnEmptyLargeParkingSpotWithElectricCharger() throws ParkingSpotNotFound {
+        //given
+        when(parkingLotRepository.getEmptyParkingSpotWithElectricChargerOfType(ParkingSpotType.LARGE)).thenReturn(new LargeParkingSpot(true));
+        //when
+        ParkingSpot parkingSpot = regularUserParkingStrategy.getParkingSpot(new Truck("", true), parkingLotRepository);
+        //then
         assertEquals(true, parkingSpot.hasElectricCharger());
-    }
-
-    @Test
-    void getParkingSpotShouldThrowExceptionWhenSmallParkingSpotHasNoElectricCharger(){
-        parkingSpots.add(new SmallParkingSpot(false));
-        parkingSpots.add(new SmallParkingSpot(false));
-        parkingSpots.add(new MediumParkingSpot(true));
-        parkingSpots.add(new LargeParkingSpot(true));
-
-        assertThrows(ParkingSpotNotFound.class, ()-> regularUserParkingStrategy.getParkingSpot(new Motorcycle(PLATE_NUMBER, true),
-                parkingLotRepository));
-    }
-
-    @Test
-    void getParkingSpotShouldThrowExceptionWhenMediumParkingSpotHasNoElectricCharger(){
-        parkingSpots.add(new SmallParkingSpot(true));
-        parkingSpots.add(new SmallParkingSpot(false));
-        parkingSpots.add(new MediumParkingSpot(false));
-        parkingSpots.add(new LargeParkingSpot(true));
-
-        assertThrows(ParkingSpotNotFound.class, ()-> regularUserParkingStrategy.getParkingSpot(new Car(PLATE_NUMBER, true),
-                parkingLotRepository));
-    }
-
-    @Test
-    void getParkingSpotShouldThrowExceptionWhenLargeParkingSpotHasNoElectricCharger(){
-        parkingSpots.add(new SmallParkingSpot(true));
-        parkingSpots.add(new SmallParkingSpot(true));
-        parkingSpots.add(new MediumParkingSpot(true));
-        parkingSpots.add(new LargeParkingSpot(false));
-
-        assertThrows(ParkingSpotNotFound.class, ()-> regularUserParkingStrategy.getParkingSpot(new Truck(PLATE_NUMBER, true),
-                parkingLotRepository));
+        assertEquals(ParkingSpotType.LARGE, parkingSpot.getParkingSpotType());
+        verify(parkingLotRepository, times(1)).getEmptyParkingSpotWithElectricChargerOfType(ParkingSpotType.LARGE);
     }
 
 }
