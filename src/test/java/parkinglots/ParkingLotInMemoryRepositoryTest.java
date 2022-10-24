d37adbb4de125b47e68987c853a632d2efc416cd
Gheorghita Butnaru <gheorghita.butnaru@basware.com>
2022-10-24 15:46:44 +0300

    unit tests refactoring

--- src/test/java/parkinglots/ParkingLotInMemoryRepositoryTest.java
+++ src/test/java/parkinglots/ParkingLotInMemoryRepositoryTest.java
@@ -2,9 +2,6 @@ package parkinglots;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import parkingspots.MediumParkingSpot;
 import parkingspots.ParkingSpot;
 import parkingspots.SmallParkingSpot;
@@ -12,26 +9,18 @@ import vehicles.Car;
 import vehicles.Motorcycle;
 import vehicles.Vehicle;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 import static parkingspots.ParkingSpotType.MEDIUM;
 import static parkingspots.ParkingSpotType.SMALL;
 
 class ParkingLotInMemoryRepositoryTest {
-    @Mock
-    private List<ParkingSpot> parkingSpots;
-    @InjectMocks
-    private ParkingLotInMemoryRepository parkingLotInMemoryRepository;
-
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
-        parkingLotInMemoryRepository = new ParkingLotInMemoryRepository(parkingSpots);
     }
 
     @Test
@@ -40,8 +29,9 @@ class ParkingLotInMemoryRepositoryTest {
         Vehicle motorcycle = new Motorcycle("", false);
         smallParkingSpot.setVehicle(motorcycle);
 
-        when(parkingSpots.stream()).thenReturn(Stream.of(smallParkingSpot));
+        ParkingLotInMemoryRepository parkingLotInMemoryRepository = new ParkingLotInMemoryRepository(new ArrayList<>(Arrays.asList(smallParkingSpot)));
         Optional<ParkingSpot> parkingSpotOptional = parkingLotInMemoryRepository.findVehicle(motorcycle);
+
         assertFalse(parkingSpotOptional.get().hasElectricCharger());
         assertEquals(SMALL, parkingSpotOptional.get().getParkingSpotType());
         assertEquals(motorcycle, parkingSpotOptional.get().getVehicle());
@@ -53,8 +43,9 @@ class ParkingLotInMemoryRepositoryTest {
         Vehicle motorcycle = new Motorcycle("", true);
         smallParkingSpot.setVehicle(motorcycle);
 
-        when(parkingSpots.stream()).thenReturn(Stream.of(smallParkingSpot));
+        ParkingLotInMemoryRepository parkingLotInMemoryRepository = new ParkingLotInMemoryRepository(new ArrayList<>(Arrays.asList(smallParkingSpot)));
         Optional<ParkingSpot> parkingSpotOptional = parkingLotInMemoryRepository.findVehicle(motorcycle);
+
         assertTrue(parkingSpotOptional.get().hasElectricCharger());
         assertEquals(SMALL, parkingSpotOptional.get().getParkingSpotType());
         assertEquals(motorcycle, parkingSpotOptional.get().getVehicle());
@@ -66,7 +57,7 @@ class ParkingLotInMemoryRepositoryTest {
         Vehicle car = new Car("", true);
         mediumParkingSpot.setVehicle(car);
 
-        when(parkingSpots.stream()).thenReturn(Stream.of(mediumParkingSpot));
+        ParkingLotInMemoryRepository parkingLotInMemoryRepository = new ParkingLotInMemoryRepository(new ArrayList<>(Arrays.asList(mediumParkingSpot)));
         Optional<ParkingSpot> parkingSpotOptional = parkingLotInMemoryRepository.findVehicle(car);
         assertTrue(parkingSpotOptional.get().hasElectricCharger());
         assertEquals(MEDIUM, parkingSpotOptional.get().getParkingSpotType());
@@ -79,7 +70,7 @@ class ParkingLotInMemoryRepositoryTest {
         Vehicle motorcycle = new Motorcycle("", true);
         mediumParkingSpot.setVehicle(motorcycle);
 
-        when(parkingSpots.stream()).thenReturn(Stream.of(mediumParkingSpot));
+        ParkingLotInMemoryRepository parkingLotInMemoryRepository = new ParkingLotInMemoryRepository(new ArrayList<>(Arrays.asList(mediumParkingSpot)));
         Optional<ParkingSpot> parkingSpotOptional = parkingLotInMemoryRepository.findVehicle(motorcycle);
         assertFalse(parkingSpotOptional.get().hasElectricCharger());
         assertEquals(MEDIUM, parkingSpotOptional.get().getParkingSpotType());
@@ -88,14 +79,14 @@ class ParkingLotInMemoryRepositoryTest {
 
     @Test
     void getEmptyParkingSpotWithoutElectricChargerOfTypeSmallShouldReturnEmptyOptionalIfThereIsNoEmptySmallParkingSpotWithoutElectricCharger(){
-        when(parkingSpots.stream()).thenReturn(Stream.empty());
+        ParkingLotInMemoryRepository parkingLotInMemoryRepository = new ParkingLotInMemoryRepository(new ArrayList<>());
         Optional<ParkingSpot> parkingSpotOptional = parkingLotInMemoryRepository.getEmptyParkingSpotWithoutElectricChargerOfType(SMALL);
         assertTrue(parkingSpotOptional.isEmpty());
     }
 
     @Test
     void getEmptyParkingSpotWithoutElectricChargerOfTypeSmallShouldReturnOptionalOfSmallParkingSpotWithoutElectricChargerIfThereIsAtLeastOneEmptySmallParkingSpotWithoutElectricCharger(){
-        when(parkingSpots.stream()).thenReturn(Stream.of(new SmallParkingSpot(false)));
+        ParkingLotInMemoryRepository parkingLotInMemoryRepository = new ParkingLotInMemoryRepository(new ArrayList<>(Arrays.asList(new SmallParkingSpot(false))));
         Optional<ParkingSpot> parkingSpotOptional = parkingLotInMemoryRepository.getEmptyParkingSpotWithoutElectricChargerOfType(SMALL);
         assertTrue(parkingSpotOptional.isPresent());
         assertEquals(SMALL, parkingSpotOptional.get().getParkingSpotType());
@@ -104,8 +95,10 @@ class ParkingLotInMemoryRepositoryTest {
 
     @Test
     void getEmptyParkingSpotWithElectricChargerOfTypeSmallShouldReturnEmptyOptionalIfThereIsNoEmptySmallParkingSpotWithElectricCharger(){
-        when(parkingSpots.stream()).thenReturn(Stream.empty());
+        ParkingLotInMemoryRepository parkingLotInMemoryRepository = new ParkingLotInMemoryRepository(new ArrayList<>());
         Optional<ParkingSpot> parkingSpotOptional = parkingLotInMemoryRepository.getEmptyParkingSpotWithElectricChargerOfType(SMALL);
         assertTrue(parkingSpotOptional.isEmpty());
     }
+
+
 }
