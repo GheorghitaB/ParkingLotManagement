1096f05addfff7a7b1935581d0a6b9f3056c074d
Gheorghita Butnaru <gheorghita.butnaru@basware.com>
2022-11-09 10:29:53 +0200

    Class refactoring

--- src/main/java/properties/AppProperty.java
+++ src/main/java/properties/AppProperty.java
@@ -1,104 +1,55 @@
 package properties;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import Utils.Constants;
+
+import static Utils.TextArgumentParser.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AppProperty {
     private static final int NUMBER_OF_ALLOWED_ARGUMENTS = 2;
-    private static final Map<String, String> propertyPathMap;
+    private static final Map<String, String> propertiesMap;
+    private static final String SPLIT_REGEX = "=";
 
     static {
-        propertyPathMap = new HashMap<>();
-        loadProperties("properties/app-properties.init");
+        propertiesMap = new HashMap<>();
+        loadPropertiesFromFile();
     }
 
-    private static void loadProperties(String appPropertyFilePath){
-        InputStream is = null;
-        InputStreamReader isr = null;
-        BufferedReader br = null;
-
-        try{
-            is = AppProperty.class.getClassLoader().getResourceAsStream(appPropertyFilePath);
-            if (is == null){
-                throw new FileNotFoundException("Resource file path \"" + appPropertyFilePath + "\" has not been found");
-            }
-            isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-            br = new BufferedReader(isr);
-            String line;
-
-            while ((line = br.readLine()) != null){
-                String[] arguments = line.split("=");
-                validateArguments(arguments);
-                arguments = prepareArguments(arguments);
-                addPropertyToMap(arguments);
-            }
-
-        } catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-            System.exit(-1);
-        } catch (IOException e) {
-            System.out.println("Error while reading the application property file path \"" + appPropertyFilePath + "\"");
-            System.exit(-1);
-        } finally {
-            closeStreams(is, isr, br);
-        }
-    }
-
-    private static String[] prepareArguments(String[] arguments) {
-        return Arrays.stream(arguments)
-                .map(String::strip)
-                .toArray(String[]::new);
-    }
-
-    private static void closeStreams(InputStream is, InputStreamReader isr, BufferedReader br){
-        try {
-            if(is != null){
-                is.close();
-            }
-            if(isr != null){
-                isr.close();
-            }
-            if(br != null){
-                br.close();
-            }
-        } catch (IOException e) {
-            System.out.println("Stream could not be closed");
-        }
+    private static void loadPropertiesFromFile(){
+        List<String> lines = readLines(Constants.APP_PROPERTIES_FILEPATH);
+        lines.forEach(line -> {
+            String[] arguments = getArgumentsFromLine(line, SPLIT_REGEX);
+            validateArguments(arguments);
+            arguments = prepareArguments(arguments);
+            addPropertyToMap(arguments);
+        });
     }
 
     private static void addPropertyToMap(String[] arguments) {
-        propertyPathMap.put(arguments[0], arguments[1]);
+        propertiesMap.put(arguments[0], arguments[1]);
     }
 
     private static void validateArguments(String[] arguments) {
-        try{
-            validateNumberOfArguments(arguments);
-        } catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            System.exit(-1);
-        }
+        validateNumberOfArguments(arguments);
     }
 
     private static void validateNumberOfArguments(String[] arguments){
         if(arguments.length != 2){
-            throw new IllegalArgumentException("Illegal number of arguments. It must be " + NUMBER_OF_ALLOWED_ARGUMENTS
-                                                + ", but it was " + arguments.length);
+            System.out.println("Illegal number of arguments. It must be " + NUMBER_OF_ALLOWED_ARGUMENTS
+                    + ", but it was " + arguments.length);
+            System.exit(-1);
         }
     }
 
-    public static String getResourcePath(String property) {
-        String resourcePath = propertyPathMap.get(property);
-        try{
-            if(resourcePath == null){
-                throw new FileNotFoundException("Resource path of \"" + property + "\" has not been found");
-            }
-        } catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
+    public static String getProperty(String property) {
+        String propertyValue = propertiesMap.get(property);
+        if(propertyValue == null){
+            System.out.println("Resource path of \"" + property + "\" has not been found");
             System.exit(-1);
         }
-        return resourcePath;
+
+        return propertyValue;
     }
 }
