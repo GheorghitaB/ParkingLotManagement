142a50d4d6d11519f59c522c79b1d478ccf6c485
Gheorghita Butnaru <gheorghita.butnaru@basware.com>
2022-11-09 10:32:06 +0200

    Added constants

---
+++ src/main/java/Utils/Constants.java
@@ -0,0 +1,8 @@
+package Utils;
+
+public class Constants {
+    public static final String REGULAR_USER_STRATEGY_FILEPATH = "regular-user-strategy";
+    public static final String VIP_USER_STRATEGY_FILEPATH = "vip-user-strategy";
+    public static final String PARKING_SPOTS_FILEPATH = "parking-spots";
+    public static final String APP_PROPERTIES_FILEPATH = "properties/app-properties.init";
+}
