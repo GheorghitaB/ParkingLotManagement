e080248635eb69a1121e5dbc8f578196e4f1acc9
Gheorghita Butnaru <gheorghita.butnaru@basware.com>
2022-11-15 13:26:50 +0200

    Added unit test

--- src/test/java/inits/parkingstrategies/RegularUserStrategyInitTest.java
+++ src/test/java/inits/parkingstrategies/RegularUserStrategyInitTest.java
@@ -150,9 +150,32 @@ class RegularUserStrategyInitTest {
             textArgumentParserMockedStatic.when( () -> TextArgumentParser.getArgumentsFromLine(line)).thenReturn(arguments);
             textArgumentParserMockedStatic.when( () -> TextArgumentParser.prepareArguments(arguments)).thenReturn(arguments);
             argumentValidatorMockedStatic.when( () -> ArgumentValidator.validateNumberOfArguments(arguments, RegularUserStrategyInit.NUMBER_OF_ARGUMENTS)).thenReturn(true);
-            argumentValidatorMockedStatic.when( () -> ArgumentValidator.validateVehicleTypeArgument(vehicleType)).thenReturn(true);
-            argumentValidatorMockedStatic.when( () -> ArgumentValidator.validateParkingSpotTypeArgument(parkingSpotType)).thenReturn(true);
         });
+        argumentValidatorMockedStatic.when( () -> ArgumentValidator.validateVehicleTypeArgument(vehicleType)).thenReturn(true);
+        argumentValidatorMockedStatic.when( () -> ArgumentValidator.validateParkingSpotTypeArgument(parkingSpotType)).thenReturn(true);
+
+        Map<VehicleType, ParkingSpotType> result = RegularUserStrategyInit.getMapOfFittingParkingSpotsFromResource(RESOURCE_PATH_MOCK);
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void getMapOfFittingParkingSpotsFromResource_ShouldReturnMapOfSizeOneWhenThereAreTwoLinesAndOneLineIsCommented(){
+        String line1 = "MOTORCYCLE SMALL";
+        String line2 = TextArgumentParser.DEFAULT_COMMENT_PREFIX + "CAR MEDIUM";
+        List<String> lines = List.of(line1, line2);
+        String[] argumentsLine1 = line1.split(TextArgumentParser.DEFAULT_SPLIT_REGEX);
+
+        textArgumentParserMockedStatic.when( () -> TextArgumentParser.readLines(RESOURCE_PATH_MOCK)).thenReturn(lines);
+
+        lines.forEach(line -> textArgumentParserMockedStatic.when( () -> TextArgumentParser.prepareLine(line)).thenReturn(line));
+        textArgumentParserMockedStatic.when( () -> TextArgumentParser.notComment(line1)).thenReturn(true);
+        textArgumentParserMockedStatic.when( () -> TextArgumentParser.notComment(line2)).thenReturn(false);
+        textArgumentParserMockedStatic.when( () -> TextArgumentParser.getArgumentsFromLine(line1)).thenReturn(argumentsLine1);
+        textArgumentParserMockedStatic.when( () -> TextArgumentParser.prepareArguments(argumentsLine1)).thenReturn(argumentsLine1);
+        argumentValidatorMockedStatic.when( () -> ArgumentValidator.validateNumberOfArguments(argumentsLine1, RegularUserStrategyInit.NUMBER_OF_ARGUMENTS)).thenReturn(true);
+
+        argumentValidatorMockedStatic.when( () -> ArgumentValidator.validateVehicleTypeArgument(VehicleType.MOTORCYCLE.name())).thenReturn(true);
+        argumentValidatorMockedStatic.when( () -> ArgumentValidator.validateParkingSpotTypeArgument(ParkingSpotType.SMALL.name())).thenReturn(true);
 
         Map<VehicleType, ParkingSpotType> result = RegularUserStrategyInit.getMapOfFittingParkingSpotsFromResource(RESOURCE_PATH_MOCK);
         assertEquals(1, result.size());
