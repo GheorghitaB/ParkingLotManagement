cfd4c4097c121d727b53c4645e97f14a390eaa7b
Gheorghita Butnaru <gheorghita.butnaru@basware.com>
2022-11-21 15:01:17 +0200

    Signature changed

---
+++ src/main/java/services/parkings/lots/ParkingLotManager.java
@@ -0,0 +1,49 @@
+package services.parkings.lots;
+
+import exceptions.ParkingSpotNotFound;
+import exceptions.ParkingSpotTypeNotFoundException;
+import exceptions.UserTypeNotFoundException;
+import exceptions.VehicleTypeNotFoundException;
+import models.parkings.spots.ParkingSpot;
+import services.parkings.spots.ParkingSpotService;
+import models.tickets.Ticket;
+import models.users.User;
+import models.vehicles.Vehicle;
+import services.parkings.strategies.ParkingStrategy;
+import services.parkings.strategies.ParkingStrategyFactory;
+import services.taxes.ParkingPriceCalculator;
+
+import java.util.Optional;
+
+public class ParkingLotManager{
+	private final ParkingSpotService parkingSpotService;
+	private final ParkingStrategyFactory parkingStrategyFactory;
+	private final ParkingPriceCalculator parkingPriceCalculator;
+
+	public ParkingLotManager(ParkingSpotService parkingSpotService, ParkingStrategyFactory parkingStrategyFactory, ParkingPriceCalculator parkingPriceCalculator) {
+		this.parkingSpotService = parkingSpotService;
+		this.parkingStrategyFactory = parkingStrategyFactory;
+		this.parkingPriceCalculator = parkingPriceCalculator;
+	}
+
+	public Ticket park(int parkingDurationInMinutes, User user, Vehicle vehicle)
+			throws ParkingSpotNotFound, VehicleTypeNotFoundException, UserTypeNotFoundException, ParkingSpotTypeNotFoundException {
+		Optional<ParkingSpot> parkingSpotOptional = getParkingSpot(user, vehicle);
+		if(parkingSpotOptional.isPresent()){
+			parkingSpotOptional.get().setVehicle(vehicle);
+			System.out.println(parkingPriceCalculator.getTotalPrice(parkingDurationInMinutes, user, vehicle, parkingSpotOptional.get()));
+			return new Ticket(user, vehicle, parkingSpotOptional.get());
+		}
+
+		throw new ParkingSpotNotFound("Parking spot not found exception");
+	}
+
+	private Optional<ParkingSpot> getParkingSpot(User user, Vehicle vehicle) {
+		ParkingStrategy parkingStrategy = parkingStrategyFactory.getParkingStrategy(user);
+		return parkingStrategy.getParkingSpot(vehicle, parkingSpotService);
+	}
+
+	public Optional<ParkingSpot> findVehicleByPlateNumber(String plateNumber) {
+		return parkingSpotService.findVehicleByPlateNumber(plateNumber);
+	}
+}
