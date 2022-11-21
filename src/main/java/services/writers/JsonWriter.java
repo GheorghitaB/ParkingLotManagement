edac9a4b8c09d625880bbd664428edff4ead53f0
Gheorghita Butnaru <gheorghita.butnaru@basware.com>
2022-11-21 14:59:23 +0200

    Added Json Writer

---
+++ src/main/java/services/writers/JsonWriter.java
@@ -0,0 +1,31 @@
+package services.writers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.*;
+
+import java.util.List;
+
+public class JsonWriter<T> {
+    public void writeOrAppendAsArray(File file, T object) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        if(file.length() == 0) {
+            objectMapper.writeValue(file, List.of(object));
+        }
+         else {
+            String jsonObjectAsString = objectMapper.writeValueAsString(object);
+            Object newObject = objectMapper.readValue(jsonObjectAsString, Object.class);
+
+            objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+            List<Object> values = objectMapper.readValue(file, new TypeReference<>() {});
+            values.add(newObject);
+
+            objectMapper.writeValue(file, values);
+        }
+    }
+}
