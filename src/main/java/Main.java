bbcf78aa86ea9a052f63ec6f2bb6d27b35b2b602
Gheorghita Butnaru <gheorghita.butnaru@basware.com>
2022-11-22 09:51:06 +0200

    Code refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,74 +28,62 @@ import java.util.Optional;
 
 
 public class Main {
+
+    private static ParkingLotManager parkingLotManager;
+
     public static void main(String[] args) {
         List<ParkingSpot> parkingSpotList = ParkingSpotsInit.getListOfParkingSpotsFromResource(AppProperty.getProperty(Constants.PARKING_SPOTS_FILEPATH_PROPERTY));
         ParkingSpotService parkingSpotService = new ParkingSpotInMemoryService(parkingSpotList);
         ParkingPriceCalculator parkingPriceCalculator = new ParkingPriceCalculatorImpl(new UserTypePriceImpl(), new VehicleTypePriceImpl(), new ParkingSpotTypePriceImpl(), new DiscountCalculatorImpl());
-        ParkingLotManager parkingLotManager = new ParkingLotManager(parkingSpotService, ParkingStrategyFactory.getParkingStrategyInstance(), parkingPriceCalculator);
+        parkingLotManager = new ParkingLotManager(parkingSpotService, ParkingStrategyFactory.getParkingStrategyInstance(), parkingPriceCalculator);
+
         File ticketSavingLocation = new File(System.getProperty("user.dir") + "/" + AppProperty.getProperty(Constants.TICKET_SAVING_LOCATION));
         List<Ticket> tickets = new ArrayList<>();
 
-        User user = new VIPUser("John Smith");
-        Vehicle vehicle = new Motorcycle("22BB", true);
-        int parkingDuration = 300;
-
-        User anotherUser = new RegularUser("NoName");
-        Vehicle anotherVehicle = new Motorcycle("22BC", true);
-
         try {
-            Ticket ticket = parkingLotManager.park(parkingDuration, user, vehicle);
-            //new JsonWriter<Ticket>().writeOrAppendAsArray(ticketSavingLocation, ticket);
-            tickets.add(ticket);
+            Ticket ticket1 = park(new VIPUser("John Smith"), new Motorcycle("22BB", true), 300);
+            Ticket ticket2 = park(new RegularUser("NoName"), new Motorcycle("22BC", true), 250);
+
+            tickets.add(ticket1);
+            tickets.add(ticket2);
+
+            displayWhereVehicleIsParked("22BB");
+            displayWhereVehicleIsParked("22BC");
+
+            writeTickets(tickets, ticketSavingLocation);
+
         } catch (ParkingSpotNotFound e) {
-            System.out.println("Parking spot unavailable (not found) for user type " + user.getUserType()
-                                    + " with vehicle type " + vehicle.getVehicleType());
+            System.out.println("There are not available parking spots right now. (" + e.getMessage() + ")");
         } catch (VehicleTypeNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("Vehicle type has not been found. (" + e.getMessage() + ")");
         } catch (UserTypeNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("User type has not been found. (" + e.getMessage() + ")");
         } catch (ParkingSpotTypeNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("Parking spot type has not been found. (" + e.getMessage() + ")");
         }
+    }
 
-        Optional<ParkingSpot> parkingSpotOptional = parkingLotManager.findVehicleByPlateNumber(vehicle.getPlateNumber());
-        if(parkingSpotOptional.isPresent()){
-            System.out.println(vehicle.getPlateNumber() + " is parked on parking spot id " + parkingSpotOptional.get().getId()
-                    + " (" + parkingSpotOptional.get().getParkingSpotType()+")");
+    private static void displayWhereVehicleIsParked(String plateNumber){
+        Optional<ParkingSpot> parkingSpotOptional = parkingLotManager.findVehicleByPlateNumber(plateNumber);
+        if(parkingSpotOptional.isEmpty()){
+            System.out.println("The vehicle with plate number: " + plateNumber + " is not parked.");
         } else {
-            System.out.printf("Parking spot for plate number %s has not been found.\n", vehicle.getPlateNumber());
+            System.out.println("The vehicle with plate number: " + plateNumber + " is parked on parking spot "
+                                    + parkingSpotOptional.get());
         }
+    }
 
-        System.out.println();
+    private static Ticket park(User user, Vehicle vehicle, int parkDurationInMinutes)
+            throws VehicleTypeNotFoundException, UserTypeNotFoundException, ParkingSpotTypeNotFoundException, ParkingSpotNotFound {
+        return parkingLotManager.park(parkDurationInMinutes, user, vehicle);
+    }
 
-        try {
-            Ticket anotherTicket = parkingLotManager.park(parkingDuration, anotherUser, anotherVehicle);
-            //new JsonWriter<Ticket>().writeOrAppendAsArray(ticketSavingLocation, anotherTicket);
-            tickets.add(anotherTicket);
-        } catch (ParkingSpotNotFound e) {
-            System.out.println("Parking spot unavailable (not found) for user type " + anotherUser.getUserType()
-                    + " with vehicle type " + anotherVehicle.getVehicleType());
-        } catch (VehicleTypeNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (UserTypeNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (ParkingSpotTypeNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        Optional<ParkingSpot> anotherParkingSpotOptional = parkingLotManager.findVehicleByPlateNumber(anotherVehicle.getPlateNumber());
-        if(anotherParkingSpotOptional.isPresent()){
-            System.out.println(anotherVehicle.getPlateNumber() + " is parked on parking spot id " + anotherParkingSpotOptional.get().getId()
-                    + " (" + anotherParkingSpotOptional.get().getParkingSpotType()+")");
-        } else {
-            System.out.printf("Parking spot for plate number %s has not been found.\n", anotherVehicle.getPlateNumber());
-        }
-
+    private static void writeTickets(List<Ticket> tickets, File ticketSavingLocation){
         JsonWriter<Ticket> jsonWriter = new JsonWriter<>();
         try {
             jsonWriter.write(ticketSavingLocation, tickets);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("The tickets could not be written");
         }
     }
 }
